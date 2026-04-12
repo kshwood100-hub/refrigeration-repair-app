b3792bb4803d53a58562b389c2a1c49962490630
kshwood100 <kshwood100@gmail.com>
2026-04-12 11:10:20 +0900

    debug: show all env keys

--- api/test.js
+++ api/test.js
@@ -1,6 +1,8 @@
 export default function handler(req, res) {
+  const keys = Object.keys(process.env).filter(k => !k.startsWith('npm_') && !k.startsWith('NODE'))
   res.status(200).json({
     hasGeminiKey: !!process.env.GEMINI_API_KEY,
     keyLength: process.env.GEMINI_API_KEY?.length ?? 0,
+    allKeys: keys.sort(),
   })
 }
